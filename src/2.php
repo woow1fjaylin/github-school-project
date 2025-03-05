fb0f6764fa7b8891788a8a38fe8b4b58fadbe492
woow1fjaylin <woow1fjaylin@gmx.com>
2025-03-05 16:18:13 +0000

    Add 2.php

---
+++ src/2.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+// Function to generate a random number between 1 and 10
+function get_random_number() {
+    return rand(1, 10);
+}
+
+// Function to calculate the sum of two numbers
+function sum($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Function to check if a number is even or odd
+function is_even($num) {
+    return ($num % 2 == 0);
+}
+
+?>
